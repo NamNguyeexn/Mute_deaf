9729f3bf0efef181853fc9de42da5252d936f02a
Nguyễn Nam <montoan01102002@gmail.com>
2023-10-20 13:51:45 +0700

    commit

---
+++ src/main/java/com/business/MuteDeafApplication.java
@@ -0,0 +1,13 @@
+package com.business;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuteDeafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuteDeafApplication.class, args);
+    }
+
+}
